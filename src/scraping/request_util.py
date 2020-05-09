bb2edba0c928bf9094101e554fbc80ca57c4ed5b
Jill Augustine <37906581+jill-augustine@users.noreply.github.com>
2020-05-09 19:47:16 +0200

    fix NameError in make_request
    fix NameError in make_request, migrate filter_response() from main.py, handle responses of None

--- src/scraping/request_util.py
+++ src/scraping/request_util.py
@@ -16,6 +16,7 @@ def make_request(url):
     :type url: str
     :return: response
     """
+    resp = None
     try:
         resp = req.get(url, timeout=3)
         resp.raise_for_status()
@@ -23,9 +24,35 @@ def make_request(url):
         print("Http Error:", ehttp)
     except req.exceptions.ConnectionError as econ:
         print("Error Connecting:", econ)
-    except requests.exceptions.Timeout as etime:
+    except req.exceptions.Timeout as etime:
         print("Timeout Error:", etime)
-    except requests.exceptions.RequestException as ereq:
+    except req.exceptions.RequestException as ereq:
         print("OOps: Something Else", ereq)
 
     return resp
+
+def filter_response(resp):
+    """ Filters requests according to desired schema.
+
+    :param resp: requests.Response
+    :return: Filtered data
+    :return type: dict
+    """
+    data = {}
+    
+    if resp is not None:
+        json_resp = resp.json()
+
+        if json_resp["title"]:
+            data["title"] = json_resp["title"]
+        else:
+            data["title"] = None
+
+        if json_resp["text"]:
+            data["text"] = json_resp["text"]
+        else:
+            data["text"] = None
+    
+    return data
+
+    
