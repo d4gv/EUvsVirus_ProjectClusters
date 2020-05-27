6eeb5d1d4630aebdf4ba2537f87b645143636772
datadonK23 <datadonk23@gmail.com>
2020-05-27 18:22:12 +0200

    Update main.py according to code review hints.

--- src/scraping/main.py
+++ src/scraping/main.py
@@ -16,70 +16,99 @@ Script crawls endpoints provided by devpost-api project by ViRb3
 Data are persisted as JSON dump in data directory.
 
 Authors: jill-augustine, datadonk23
-Date: 07.05.20 
+Date: 07.05.20
 """
 
 import os
 import json
 import logging
+import pandas as pd
+from request_util import make_request, filter_response
+
 logging.getLogger().setLevel(logging.INFO)
 
-from request_util import make_request, filter_response
-import pandas as pd
+HOST = "http://127.0.0.1:5000"
+PROJECT_ENDPOINT = "/project/"
 
-def get_filtered_data(proj):
-    '''Given a project name, returns filtered response and success indicator
-    
-    :param proj: project name
-    :type proj: str
-    :return: tuple of response and success indicator'''
-    logging.info("Fetch data from " + str(proj))
 
-    req_url = host + project_endpoint + proj
-    resp = make_request(req_url)
-
-    filtered_response = filter_response(resp)
-    filtered_response['proj_url'] = "https://devpost.com/software/"+proj
-
-    success =  True if resp is not None else False
-    
-    return (filtered_response, success)
-
-def perist_fetched_data(fetched_data, f_path):
-    """ Persist fetched data as JSON file.
-
-    :param f_path: Filepath of output file
-    :type f_path: str
-    :param fetched_data: Fetched data
-    :type fetched_data: [Dict]
-    :return: -
+def get_filtered_data(request_url, project):
     """
-    with open(f_path, "w") as f:
-        json.dump(fetched_data, f)
+    Given a project name, returns filtered response and success indicator.
+
+    :param request_url: the url to make the request to
+    :type request_url:  str
+    :param project:     project name
+    :type project:      str
+    :return:            tuple of response and success indicator
+    """
+    logging.info("Fetch data from %s", str(project))
+
+    response = make_request(request_url + project)
+
+    filtered_response = filter_response(response)
+    filtered_response["proj_url"] = "https://devpost.com/software/" + project
+
+    return (filtered_response, response is not None)
+
+
+def persist_fetched_data(fetched_data, file_path):
+    """
+    Persist fetched data as JSON file.
+
+    :param file_path:    Filepath of output file
+    :type file_path:     str
+    :param fetched_data: Fetched data
+    :type fetched_data:  [Dict]
+    :return:             -
+    """
+    with open(file_path, "w") as output_file:
+        json.dump(fetched_data, output_file)
 
 
 if __name__ == "__main__":
-    host = "http://127.0.0.1:5000"
-    project_endpoint = "/project/"
-    fetched_data = []
-    data_dir = os.path.join(os.environ["HOME"],"Dropbox","EUvsVirus")
-    output_fpath = os.path.join(data_dir,"EUvsVirus_projects.json")
+    """ Main program loop """
 
-    projectURLS_df = pd.read_csv(os.path.join(data_dir,"projectURLS.csv"))
-    proj_names = projectURLS_df.ProjURL.str.extract(r"(?<=https://devpost.com/software/)(.+$)",expand=False).tolist()
+    # prepare project endpoints
+    request_url = HOST + PROJECT_ENDPOINT
 
-    all_responses = [None for x in range(len(proj_names))]
-    for i in range(len(proj_names)):
-        print(f"Project {i+1} of {len(proj_names)}")
-        all_responses[i] = get_filtered_data(proj_names[i])
+    data_dir = os.path.join(os.environ.get("HOME"), "Dropbox", "EUvsVirus")
+    output_file_path = os.path.join(data_dir, "EUvsVirus_projects.json")
 
-    perist_fetched_data([x[0] for x in all_responses],output_fpath)
+    project_urls_df = pd.read_csv(os.path.join(data_dir, "projectURLS.csv"))
+    project_names = (
+        project_urls_df
+        .ProjURL
+        .str.extract(
+            r"(?<=https://devpost.com/software/)(.+$)", expand=False)
+        .tolist())
+    projects = len(project_names)
 
-    all_responses_df = pd.DataFrame.from_records(all_responses, columns=['Response','Success'])
-    print(f'{all_responses_df.Success.sum()} of {len(all_responses_df)} URLs were successfully scraped.')
+    # query project endpoints and persist data
+    all_responses = []
+    for i in range(projects):
+        logging.info("Project %s of %s", str(i + 1), str(projects))
+        all_responses.append(get_filtered_data(request_url, project_names[i]))
 
-    all_data = (projectURLS_df.merge(all_responses_df.Response.apply(pd.Series), 
-                                    left_on='ProjURL', right_on='proj_url', how='left')
-                              .drop(columns='proj_url')
-    )
-    all_data.to_csv(os.path.join(data_dir,'all_data.tsv'), sep='\t', index=False)
+    persist_fetched_data([x[0] for x in all_responses], output_file_path)
+
+    all_responses_df = (
+        pd.DataFrame
+        .from_records(all_responses, columns=["Response", "Success"]))
+    logging.info(
+        "%s of %s URLs were successfully scraped.",
+        str(all_responses_df.Success.sum()),
+        str(len(all_responses_df)))
+
+    # write results
+    all_data = (
+        project_urls_df
+        .merge(
+            all_responses_df.Response.apply(pd.Series),
+            left_on="ProjURL",
+            right_on="proj_url",
+            how="left")
+        .drop(columns="proj_url"))
+    all_data.to_csv(
+        os.path.join(data_dir, "all_data.tsv"),
+        sep="\t",
+        index=False)
